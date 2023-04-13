b854d10dd05594cff785e410c2dc8b082d5f3860
WEI-88-WEI <w2395816280@gmail.com>
2023-04-13 13:46:04 +0800

    11

---
+++ 005-duyi/011-Flip动画/index.js
@@ -0,0 +1,52 @@
+// 没得源码,自己写的
+class Flip {
+  constructor(nodeList, time) {
+    this.nodeList = nodeList;
+    this.time = time;
+    this.startP = Array.from(this.nodeList.children).reduce((acc, cur) => {
+      const boxSize = cur.getBoundingClientRect();
+      // push的返回值是数组的新长度
+      acc.push({
+        top: boxSize.top,
+        height: boxSize.height,
+        nodeName: cur.innerText,
+        node: cur,
+      });
+      return acc;
+    }, []);
+  }
+  play() {
+    const endP = Array.from(this.nodeList.children).reduce((acc, cur) => {
+      const boxSize = cur.getBoundingClientRect();
+      // push的返回值是数组的新长度
+      acc.push({
+        top: boxSize.top,
+        height: boxSize.height,
+        nodeName: cur.innerText,
+        node: cur,
+      });
+      return acc;
+    }, []);
+    endP.forEach((item) => {
+      this.startP.forEach((i) => {
+        if (item.nodeName === i.nodeName && item.top !== i.top) {
+          item.node.style.transition = `none`;
+          item.node.style.transform = `translateY(${i.top - item.top}px)`;
+        }
+      });
+      // requestAnimationFrame(() => {
+      //   this.startP.forEach((item) => {
+      //     item.node.style.transition = `all ${this.time}s ease`;
+      //     item.node.style.transform = ``;
+      //   });
+      // });
+      this.timer = setTimeout(() => {
+        this.startP.forEach((item) => {
+          item.node.style.transition = `all ${this.time}s ease`;
+          item.node.style.transform = ``;
+        });
+      }, this.time * 1000);
+    });
+    this.startP = Object.assign(endP);
+  }
+}
